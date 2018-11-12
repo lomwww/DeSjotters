1803481029e011f34f8a1941783db7f21bc39087
Lom Wijnen <lomwijnen@live.be>
2018-11-12 12:56:32 +0100

    no message

--- gap-app/index.js
+++ gap-app/index.js
@@ -37,3 +37,50 @@ request('https://geodata.antwerpen.be/arcgissql/rest/services/P_Portal/portal_pu
 
   }
 );
+
+//landing page
+app.get("/", function(req, res){
+  res.render("inlog");
+});
+
+//next page
+app.get("/starwars", function(req, res){
+  res.render("starwars");
+});
+
+//
+app.get("/avatar", function(req, res){
+  res.render("avatar");
+});
+
+app.get("/wolverine", function(req, res){
+  res.render("wolverine");
+});
+
+app.get("/news", function(req, res){
+  res.render("news");
+});
+
+app.get("/oscars", function(req, res){
+  res.render("oscars");
+});
+
+app.get("/supergirl", function(req, res){
+  res.render("supergirl");
+});
+
+app.get("/chucky", function(req, res){
+  res.render("chucky");
+});
+
+app.get("/top", function(req, res){
+  res.render("top");
+});
+
+app.get("/reviews", function(req, res){
+  res.render("reviews");
+});
+
+app.get("/upcoming", function(req, res){
+  res.render("upcoming");
+});
