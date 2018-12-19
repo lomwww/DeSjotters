7280e1e1c7e22751ce0e87fbfd5428cd449c5132
Lom Wijnen <lomwijnen@live.be>
2018-12-19 12:48:19 +0100

    no message

--- gap-app/index.js
+++ gap-app/index.js
@@ -28,38 +28,31 @@ var data;
 request('https://geodata.antwerpen.be/arcgissql/rest/services/P_Portal/portal_publiek/MapServer/643/query?where=1%3D1&outFields=naam,straat,huisnummer,postcode,district,OBJECTID,id&outSR=4326&f=json',
   function(error, response, body){
     data = JSON.parse(body);
-
+/*
     for(var i=0; i < data.features.length; i++) {
         /*console.log("naam: " + data.features[i].attributes.naam);
         console.log("coord: " + data.features[i].geometry.x + ", " + data.features[i].geometry.y);
-        console.log("");*/
+        console.log("");
         console.log(data.features[i].attributes);
-    }
+    }*/
   }
 );
 
 
-var data;
-request('https://geodata.antwerpen.be/arcgissql/rest/services/P_Portal/portal_publiek/MapServer/643/query?where=1%3D1&outFields=naam,straat,huisnummer,postcode,district,OBJECTID,id&outSR=4326&f=json',
-  function(error, response, body){
-    data = JSON.parse(body);
+var eigenJSON = require("./data/eigen.json");
 
-    for(var i=0; i < data.features.length; i++) {
-        /*console.log("naam: " + data.features[i].attributes.naam);
-        console.log("coord: " + data.features[i].geometry.x + ", " + data.features[i].geometry.y);
-        console.log("");*/
-        console.log(data.features[i].attributes);
-    }
+function jos() {
+  for (var i = 0; i < eigenJSON.length; i++) {
+    console.log(eigenJSON.sporthallen)
   }
-);
-
-
+}
 
 
 
 app.get('/zalenLom', function(req, res){
   res.render('zalenLom', {
     zalen: data,
+    mijnInformatie: eigenJSON
   });
 });
 
